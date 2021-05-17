b0336416a1e951d6757f80e02d087c22d9df0eab
ayushi-2002 <80891792+ayushi-2002@users.noreply.github.com>
2021-05-17 11:22:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/" , function(req, res){
+    res.sendFile(__dirname+"/bmi_calc.html");
+});
+
+app.post("/" , function(req, res){
+    var wt = Number(req.body.weight);
+    var ht = Number(req.body.height);
+    var BMI = wt/(ht*ht);
+    res.send("Your BMI is "+ BMI);
+});
+
+app.listen(1500,function(){
+    console.log("Server started!!!");
+});
+
